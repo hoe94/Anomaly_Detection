30525a0dfb611199db43cc559c865c86fe839dde
Hoe <lkh_bear94@hotmail.com>
2022-05-05 08:48:31 +0800

    05052022 changes

---
+++ main.py
@@ -0,0 +1,47 @@
+import json
+from flask import Flask, send_file, render_template
+from google.oauth2 import service_account
+from google.cloud import bigquery
+import plotly
+import plotly.express as px
+
+#Google Big Query Credentials
+gbq_table = "Dataset.mbb"
+gcp_project_id = "tsf-project-344410"
+gbq_destination = f'{gcp_project_id}.{gbq_table}'
+credentials = service_account.Credentials.from_service_account_file("./config/GCP_TSF_ServiceAccount.json")
+bigquery_client = bigquery.Client(credentials = credentials)
+
+
+
+
+
+# SQL Query.
+query_string = """
+SELECT *
+FROM `tsf-project-344410.Dataset.mbb` 
+WHERE Date >= '2020-03-18'
+ORDER BY Date ASC
+"""
+
+app = Flask(__name__, template_folder = './templates')
+df = (
+    bigquery_client.query(query_string)
+    .result()
+    .to_dataframe(create_bqstorage_client = True,)
+)
+
+@app.route('/')
+def index():
+     return render_template('layout.html')
+
+@app.route('/graph')
+def graph():
+    
+    fig = px.line(df, x = 'Date', y = 'Close', title = 'Maybank Stock Price Line Graph')
+    fig_JSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
+    return render_template('layout.html', fig1 = fig_JSON)
+
+
+if __name__ == "__main__":
+    app.run(debug = True)
---
+++ src/test.py
@@ -0,0 +1,29 @@
+import json
+from google.oauth2 import service_account
+from google.cloud import bigquery
+import plotly
+import plotly.express as px
+
+#Google Big Query Credentials
+gbq_table = "Dataset.mbb"
+gcp_project_id = "tsf-project-344410"
+gbq_destination = f'{gcp_project_id}.{gbq_table}'
+credentials = service_account.Credentials.from_service_account_file("./config/GCP_TSF_ServiceAccount.json")
+bigquery_client = bigquery.Client(credentials = credentials)
+
+query_string = """
+SELECT *
+FROM `tsf-project-344410.Dataset.mbb` 
+WHERE Date >= '2020-03-18'
+ORDER BY Date ASC
+"""
+
+df = (
+    bigquery_client.query(query_string)
+    .result()
+    .to_dataframe(create_bqstorage_client = True,)
+)
+
+fig = px.line(df, x = 'Date', y = 'Close', title = 'Maybank Stock Price Line Graph')
+fig_JSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
+print(json.loads(fig_JSON))
